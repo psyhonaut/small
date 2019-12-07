29f9ad136e8089bfdf58f4704b2754cbe5635c22
Sergey <kossykh.sergey@gmail.com>
2019-12-07 14:49:37 +0600

    create model

---
+++ database/migrations/2019_12_07_082248_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('tickets', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->unsignedBigInteger('department_id');
+          $table->unsignedBigInteger('user_id');
+          $table->string('title');
+          $table->text('description');
+          $table->boolean('active')->default(true);
+          $table->timestamps();
+
+          $table->foreign('department_id')->references('id')->on('departments');
+          $table->foreign('user_id')->references('id')->on('users');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
